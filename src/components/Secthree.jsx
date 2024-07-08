e7f150545046b8e399d455e1ea20418811cc4786
patrickashi <134015857+patrickashi@users.noreply.github.com>
2024-07-08 17:06:18 +0100

    new
    add new

--- src/components/Secthree.jsx
+++ src/components/Secthree.jsx
@@ -2,7 +2,7 @@ import React from "react"
 
 const Secthree = () => {
     return (
-        <div className="bg-[#D6CEAD] h-screen w-full pt-32 px-2 md:px-10 lg:px-16">
+        <div className="bg-[#D6CEAD] w-full pt-32 pb-24 px-2 md:px-10 lg:px-16">
             <h2 className="mb-14 font-bold">US IN NUMBERS</h2>
             <div className="flex flex-col md:flex-row gap-4 text-white font-bold justify-between">
                 <div className="flex justify-center flex-col text-center bg-[#2C2713] py-20 px-20 rounded-md">
